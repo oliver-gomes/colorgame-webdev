22d6253253f5a1a3344675cdb453176cdc06aa32
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-11 16:39:19 -0400

    selected class added

--- colorgame.js
+++ colorgame.js
@@ -17,7 +17,7 @@ resetButton.addEventListener("click", function() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#bbb";
+  h1.style.backgroundColor = "#232323";
 });
 
 colorDisplay.textContent = pickedColor;
